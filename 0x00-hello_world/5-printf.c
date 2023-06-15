90c5ac0803bdeb303937545481bc3db3e5ad8f6a
haile12345 <antenehhayle123@gmail.com>
2023-06-15 06:43:50 -0700

    ok

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main using printf() command
+* Return: 0 prints phrase only
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
