c4d18458f5924684c6cf9825e000a34a7dc59e81
haile12345 <antenehhayle123@gmail.com>
2023-06-16 14:10:54 -0700

    is it work

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 
 	if (n == 0)
 		printf("is %d and is 0\n", n);
-	else if (n > 0 || n < 6)
+	else if (n != 0 && n < 6)
 		printf("is %d and is less than 6 and not 0\n", n);
 	else
 		printf("is %d and is greater than 6\n", n);
