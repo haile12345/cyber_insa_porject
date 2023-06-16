510304b8fbf80f4a39df5cef3880f7662e42fa36
haile12345 <antenehhayle123@gmail.com>
2023-06-16 15:41:18 -0700

    not good

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints the number form 0 - 10
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+
+	printf("\n");
+
+
+	return (0);
+}
+
