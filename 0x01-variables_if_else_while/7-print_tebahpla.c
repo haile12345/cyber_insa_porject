06e4da53bc88195567de9f42346f10c5ae73fec3
haile12345 <antenehhayle123@gmail.com>
2023-06-16 16:09:52 -0700

    ok iwilldo

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints the number form 0 - 10
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char i;
+
+
+	for (i = 122 ; i >= 97 ; i--)
+		putchar(i);
+
+	putchar("\n");
+
+
+	return (0);
+}
