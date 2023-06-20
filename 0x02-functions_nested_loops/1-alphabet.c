c514a5670aa1311b13e6033dcb0d550c506e0b2e
haile12345 <antenehhayle123@gmail.com>
2023-06-20 14:26:36 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+
+
+for (int i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+
+_putchar('\n');
+
+}
