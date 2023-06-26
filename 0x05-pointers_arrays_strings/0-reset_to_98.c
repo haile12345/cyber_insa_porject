d9d083ee93cc396ffcd9d275a06435f53cab1d48
haile12345 <antenehhayle123@gmail.com>
2023-06-26 11:54:37 -0700

    first trial:

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*swap_int - swap two integers
+*@a: first integer
+*@b: second integer
+*Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+*a, *b = *b, *a;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+
+
+void swap_int(int *a, int *b);
