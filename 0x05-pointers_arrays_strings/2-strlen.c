cc79b73cfd344976674881e097ae456c71674f9f
haile12345 <antenehhayle123@gmail.com>
2023-06-26 18:18:50 -0700

    first

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+ #include "main.h"
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s);
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+}
+
+return (i);
+
+}
