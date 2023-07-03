f04649a009e490aa288c8ca8c06d04383c4840c0
haile12345 <antenehhayle123@gmail.com>
2023-07-03 13:30:02 -0700

    why

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_
-#define MAIN_
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
+#include <stddef.h>
 char *_memset(char *s, char b, unsigned int n);
 
 
 
-#endif
+#endif /* MAIN_H*/
